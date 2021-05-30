519b577d90d66bb58948ffce5c5d3be229f823cc
myetcGit <myetc_basket@hotmail.com>
2021-05-30 18:21:48 +1000

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,18 @@
+//We can decorate class properties with Data Annotations
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandAPI.Models{
+    public class Command{
+        [Key]        
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+        [Required]
+        public string Platform { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+
+    }//class
+}//namespace
