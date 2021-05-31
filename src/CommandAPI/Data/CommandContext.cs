d9e2357b7d40d9f1bf95a33346bdd3c10cbe6ea7
myetcGit <myetc_basket@hotmail.com>
2021-05-31 22:15:07 +1000

    Added EF Core with CommandContext

---
+++ src/CommandAPI/Data/CommandContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using CommandAPI.Models;
+
+namespace CommandAPI.Data{
+    public class CommandContext : DbContext{
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options){        }
+        public DbSet<Command> CommandItems{get;set;}
+
+    }//class
+}//namespace
